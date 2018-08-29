655d21377c0688cd3153414eaf075f86e60866f9
Nick Romans <nicholas.romans@gmail.com>
2018-08-29 15:14:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 
+<<<<<<< HEAD
 #def reverse_each_word(string)
 #array = string.split
 #new_array = []
@@ -14,3 +15,22 @@ def reverse_each_word(string)
 end
   
   
+=======
+def reverse_each_word(string)
+array = string.split
+new_array = []
+array.each do |words|
+new_array << words.reverse
+end
+new_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split
+    array.collect do |words|
+    array << words.reverse
+  end
+  puts array.join(" ")
+end
+  
+>>>>>>> cc5c884d2420d0e3aeed91e970488cf5e1c20581
